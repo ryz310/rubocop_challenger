1030af9a1233def64f8181fb90692c20d43499fb
ryz310 <ryz310@gmail.com>
2018-10-14 19:57:52 +0900

    Modify cli options

--- lib/challenger/cli.rb
+++ lib/challenger/cli.rb
@@ -3,12 +3,35 @@ require 'thor'
 module Challenger
   class CLI < Thor
     desc 'rubocop_challenge', 'Run `$ rubocop -a` and create PR to GitHub'
-    option :file_path, type: :string, default: '.rubocop_todo.yml'
-    option :mode, type: :string, default: 'most_occurrence'
-    option :commiter_email, type: :string, default: 'rubocop_challenge@example.com'
-    option :commiter_name, type: :string, default: 'Rubocop Challenge'
-    option :base_branch, type: :string, default: 'master'
-    option :pull_request_labels, type: :array, default: ['rubocop challenge']
+    option :file_path,
+           type: :string,
+           default: '.rubocop_todo.yml',
+           aliases: :f,
+           desc: 'Set your ".rubocop_todo.yml" path'
+    option :mode,
+           type: :string,
+           default: 'most_occurrence',
+           desc: 'Mode to select deletion target. ' \
+                 'You can choice "most_occurrence", "least_occurrence", or "random"'
+    option :email,
+           type: :string,
+           default: 'rubocop_challenge@example.com',
+           aliases: :m,
+           desc: 'Pull Request committer email'
+    option :name,
+           type: :string,
+           default: 'Rubocop Challenge',
+           aliases: :n,
+           desc: 'Pull Request committer name'
+    option :base,
+           type: :string,
+           default: 'master',
+           desc: 'Base branch of Pull Request'
+    option :labels,
+           type: :array,
+           default: ['rubocop challenge'],
+           aliases: :l,
+           desc: 'Label to give to Pull Request'
     def rubocop_challenge
       target_rule = Rubocop::Challenge.exec(options[:file_path], options[:mode])
       PRDaikou.exec(pr_daikou_options(target_rule), nil)
@@ -18,10 +41,11 @@ module Challenger
 
     def pr_daikou_options(target_rule)
       {
-        email:  options[:commiter_email],
-        name:   options[:commiter_name],
+        email:  options[:mail],
+        name:   options[:name],
+        base:   options[:base],
         title:  target_rule.title,
-        labels: options[:pull_request_labels].join(','),
+        labels: options[:labels].join(','),
         topic:  "rubocop-challenge/#{target_rule.title.tr('/', '-')}",
         commit: ":robot: #{target_rule.title}"
       }.each_with_object({}) do |(k, v), memo|
