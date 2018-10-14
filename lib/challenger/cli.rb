17b8d551a045e76792f93def7aff7d99d6c3fbe8
ryz310 <ryz310@gmail.com>
2018-10-14 19:04:10 +0900

    Fix default label name

--- lib/challenger/cli.rb
+++ lib/challenger/cli.rb
@@ -8,7 +8,7 @@ module Challenger
     option :commiter_email, type: :string, default: 'rubocop_challenge@example.com'
     option :commiter_name, type: :string, default: 'Rubocop Challenge'
     option :base_branch, type: :string, default: 'master'
-    option :pull_request_labels, type: :array, default: ['rubocop_challenge']
+    option :pull_request_labels, type: :array, default: ['rubocop challenge']
     def rubocop_challenge
       target_rule = Rubocop::Challenge.exec(file_path, mode)
       PRDaikou.exec(
