c4bf59503abab3a085d3767705fdff6db01d20ca
ryz310 <ryz310@gmail.com>
2018-10-14 18:34:28 +0900

    Implements `rubocop_challenge` command

--- lib/challenger.rb
+++ lib/challenger.rb
@@ -3,44 +3,9 @@ require 'rubocop-rspec'
 require 'challenger/rubocop/rule'
 require 'challenger/rubocop/todo_reader'
 require 'challenger/rubocop/todo_writer'
+require 'challenger/rubocop/challenge'
 require 'challenger/version'
 require 'pr-daikou'
 
 module Challenger
-  module_function
-
-  def rubocop_challenge(rubocop_todo_file_path = nil, delete_target: 'most_occurrence')
-    rubocop_todo_file_path ||= './.rubocop_todo.yml'
-    todo_writer = Rubocop::TodoWriter.new(rubocop_todo_file_path)
-
-    target_rule = select_target_rule(rubocop_todo_file_path, delete_target)
-    todo_writer.delete_rule(target_rule)
-
-    # Run rubocop --auto-correct
-    `rubocop -a || true`
-
-    options = {
-      email: 'socialplus_admin@feedforce.jp',
-      name: 'SocialPlus Bot',
-      title: target_rule.title,
-      labels: 'ready for review, rubocop challenge',
-      topic: "rubocop-challenge/#{target_rule.title.gsub('/', '-')}",
-      commit: ":robot: #{target_rule.title}"
-    }
-
-    PRDaikou.exec(options, nil)
-  end
-
-  def select_target_rule(rubocop_todo_file_path, delete_target)
-    todo_reader = Rubocop::TodoReader.new(rubocop_todo_file_path)
-
-    case delete_target
-    when 'least_occurrence'
-      todo_reader.least_occurrence_rule
-    when 'random'
-      todo_reader.any_rule
-    else
-      todo_reader.most_occurrence_rule
-    end
-  end
 end
---
+++ lib/challenger/cli.rb
@@ -0,0 +1,27 @@
+require 'challenger'
+require 'thor'
+
+module Challenger
+  class CLI < Thor
+    desc 'rubocop_challenge', 'Run `$ rubocop -a` and create PR to GitHub'
+    option :file_path, type: :string, default: '.rubocop_todo.yml'
+    option :mode, type: :string, default: 'most_occurrence'
+    option :commiter_email, type: :string, default: 'rubocop_challenge@example.com'
+    option :commiter_name, type: :string, default: 'Rubocop Challenge'
+    option :base_branch, type: :string, default: 'master'
+    option :pull_request_labels, type: :array, default: ['rubocop_challenge']
+    def rubocop_challenge
+      target_rule = Rubocop::Challenge.exec(file_path, mode)
+      PRDaikou.exec(
+        {
+          email:  commiter_email,
+          name:   commiter_name,
+          title:  target_rule.title,
+          labels: pull_request_labels,
+          topic:  "rubocop-challenge/#{target_rule.title.tr('/', '-')}",
+          commit: ":robot: #{target_rule.title}"
+        }, nil
+      )
+    end
+  end
+end
