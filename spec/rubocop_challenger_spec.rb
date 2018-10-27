7663ff8b27a5608b6da465de996c1a8bf64d8b3a
ryz310 <ryz310@gmail.com>
2018-10-27 20:15:59 +0900

    Add README.md spec

--- spec/rubocop_challenger_spec.rb
+++ spec/rubocop_challenger_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe RubocopChallenger do
   it 'has a version number' do
     expect(RubocopChallenger::VERSION).not_to be nil
   end
+
+  describe 'README.md' do
+    let(:readme_file) { strip_whitespace(File.read('README.md')) }
+
+    def strip_whitespace(string)
+      string.lines.map(&:strip).join("\n")
+    end
+
+    it 'includes following execution result: `$ rubocop_challenger help`' do
+      execution_result = strip_whitespace(`bundle exec exe/rubocop_challenger help`)
+      expect(readme_file).to include execution_result
+    end
+
+    it 'includes following execution result: `$ rubocop_challenger help go`' do
+      execution_result = strip_whitespace(`bundle exec exe/rubocop_challenger help go`)
+      expect(readme_file).to include execution_result
+    end
+  end
 end
