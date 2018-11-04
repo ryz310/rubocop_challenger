ccddfbe6e609600613074a0181c6e942ab5ce181
ryz310 <ryz310@gmail.com>
2018-11-04 14:33:41 +0900

    Add simplecov script for Code Climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-# NOTE: If uninstalled `coveralls`, you should uncomment following codes
-# require 'simplecov'
-# SimpleCov.start
+require 'simplecov'
+SimpleCov.start
 
 require 'bundler/setup'
 require 'rubocop_challenger'
