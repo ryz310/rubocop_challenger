87010b07f42a9363ab5cd2fc7a3c83a308238e55
ryz310 <ryz310@gmail.com>
2018-11-11 00:57:15 +0900

    Modify command to execute via CommandLine class

--- lib/rubocop_challenger/rubocop/command.rb
+++ lib/rubocop_challenger/rubocop/command.rb
@@ -3,13 +3,13 @@
 module RubocopChallenger
   module Rubocop
     # To execute rubocop gem command (Mainly for mock when testing)
-    class Command
+    class Command < CommandLine
       def auto_correct
-        `bundle exec rubocop --auto-correct || true`
+        execute('bundle exec rubocop --auto-correct || true')
       end
 
       def auto_gen_config
-        `bundle exec rubocop --auto-gen-config || true`
+        execute('bundle exec rubocop --auto-gen-config || true')
       end
     end
   end
