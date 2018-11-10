c2dec0b58e25459910db14a2718313a242579d7e
ryz310 <ryz310@gmail.com>
2018-11-10 17:14:18 +0900

    Add a GitHub client class

---
+++ spec/rubocop_challenger/github/client_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe RubocopChallenger::Github::Client do
+  let(:client) { described_class.new('GITHUB_ACCESS_TOKEN', remote_url) }
+  let(:remote_url) { 'https://github.com/ryz310/rubocop_challenger.git' }
+
+  describe '#repository' do
+    context 'when use https protocol to the git remote URL' do
+      let(:remote_url) { 'https://github.com/ryz310/rubocop_challenger.git' }
+
+      it 'returns the github repository name' do
+        expect(client.repository).to eq 'ryz310/rubocop_challenger'
+      end
+    end
+
+    context 'when use git protocol to the git remote URL' do
+      let(:remote_url) { 'git@github.com:ryz310/rubocop_challenger.git' }
+
+      it 'returns the github repository name' do
+        expect(client.repository).to eq 'ryz310/rubocop_challenger'
+      end
+    end
+  end
+
+  describe '#create_pull_request' do
+    subject(:create_pull_request) do
+      client.create_pull_request(
+        base: 'base', head: 'head', title: 'title', body: 'body'
+      )
+    end
+
+    before { allow(Octokit::Client).to receive(:new).and_return(octokit_mock) }
+
+    let(:octokit_mock) do
+      instance_double(Octokit::Client, create_pull_request: sawyer_response)
+    end
+    let(:sawyer_response) { OpenStruct.new(data: response_data) }
+    let(:response_data) { OpenStruct.new(number: 123) }
+
+    it 'calls Octokit::Client#create_pull_request' do
+      create_pull_request
+      expect(octokit_mock)
+        .to have_received(:create_pull_request)
+        .with('ryz310/rubocop_challenger', 'base', 'head', 'title', 'body')
+    end
+
+    it 'returns created pull request number' do
+      expect(create_pull_request).to eq 123
+    end
+  end
+end
