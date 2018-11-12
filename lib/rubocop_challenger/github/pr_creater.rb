7bccb6f8270f3fcf91ba52ca4c66747801c573fe
ryosuke_sato <r-sato@feedforce.jp>
2018-11-12 15:14:24 +0900

    Add validation to check called #commit method

--- lib/rubocop_challenger/github/pr_creater.rb
+++ lib/rubocop_challenger/github/pr_creater.rb
@@ -8,6 +8,7 @@ module RubocopChallenger
         @topic_branch = branch
         @git = Git::Command.new(user_name: user_name, user_email: user_email)
         @github = Github::Client.new(access_token, git.remote_url)
+        @initial_sha1 = git.current_sha1
       end
 
       def commit(message)
@@ -16,20 +17,29 @@ module RubocopChallenger
         git.commit(message)
       end
 
+      # Create a pull request
+      # You should call #commit before calling this method
+      #
+      # @param title [String] Title for the pull request
+      # @param body [String] The body for the pull request
+      # @param base [String] The branch you want your changes pulled into
+      # @param labels [Array<String>] An array of labels to apply to this PR
+      # @return [Boolean] Return true if its successed
       def create_pr(title:, body:, base:, labels: [])
-        return unless git.current_branch?(topic_branch)
+        return false if git.current_sha1?(initial_sha1)
+        return false unless git.current_branch?(topic_branch)
 
         git.push('origin', topic_branch)
-
         pr_number = github.create_pull_request(
           base: base, head: topic_branch, title: title, body: body
         )
         github.add_labels(pr_number, labels)
+        true
       end
 
       private
 
-      attr_reader :git, :github, :topic_branch
+      attr_reader :git, :github, :topic_branch, :initial_sha1
     end
   end
 end
