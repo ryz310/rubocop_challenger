93ed7451f48717a46a149c440b919283dbfcfc52
ryz310 <ryz310@gmail.com>
2019-06-13 01:11:17 +0900

    Add description

--- lib/rubocop_challenger/rubocop/command.rb
+++ lib/rubocop_challenger/rubocop/command.rb
@@ -6,10 +6,15 @@ module RubocopChallenger
     class Command
       include PrComet::CommandLine
 
+      # Executes auto correction
       def auto_correct
         run('--auto-correct')
       end
 
+      # Generates `.rubocop_todo.yml`
+      #
+      # @param exclude_limit [Integer] default: nil
+      # @param auto_gen_timestamp [Boolean] default: true
       def auto_gen_config(exclude_limit: nil, auto_gen_timestamp: true)
         commands = ['--auto-gen-config']
         commands << "--exclude-limit #{exclude_limit}" if exclude_limit
