9b1c78642657c77356cebc3a640a9423380675f4
ryz310 <ryz310@gmail.com>
2019-06-22 16:56:09 +0900

    Jail break for testing

--- lib/rubocop_challenger/version.rb
+++ lib/rubocop_challenger/version.rb
@@ -1,5 +1,7 @@
-# frozen_string_literal: true
-
-module RubocopChallenger
-  VERSION = '2.0.0.pre5'
-end
+  
+    
+  module RubocopChallenger  
+    VERSION = '2.0.0.pre5'  
+  end  
+  
+puts 'hoge'; puts 'fuga'
