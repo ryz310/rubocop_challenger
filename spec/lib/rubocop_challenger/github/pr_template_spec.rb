036f8f24daaa32f8a7ac28019bdf819732d6e319
Ryosuke Sato <ryz310@gmail.com>
2023-01-03 22:03:50 +0900

    Update the specifications

--- spec/lib/rubocop_challenger/github/pr_template_spec.rb
+++ spec/lib/rubocop_challenger/github/pr_template_spec.rb
@@ -30,6 +30,11 @@ RSpec.describe RubocopChallenger::Github::PrTemplate do
         > depending on configuration.
         > It also flags uses of `alias :symbol` rather than `alias bareword`.
         >
+        > However, it will always enforce `method_alias` when used `alias`
+        > in an instance method definition and in a singleton method definition.
+        > If used in a block, always enforce `alias_method`
+        > unless it is an `instance_eval` block.
+        >
         > ### Examples
         >
         > #### EnforcedStyle: prefer_alias (default)
