38e6ac4dc474975c1b92acced45b818a920a5797
ryz310 <ryz310@gmail.com>
2024-03-01 23:31:09 +0000

    :police_car: RSpec/DescribedClass

--- spec/lib/rubocop_challenger_spec.rb
+++ spec/lib/rubocop_challenger_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe RubocopChallenger do
   it 'has a version number' do
-    expect(RubocopChallenger::VERSION).not_to be_nil
+    expect(described_class::VERSION).not_to be_nil
   end
 end
